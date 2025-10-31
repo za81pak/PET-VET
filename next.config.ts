9f3985a82eefdfe72acef687df17bb8b1b4134a6
za81pak <goodmcvpion@gmail.com>
2025-11-01 02:10:52 +0300

    resttwo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: '/PET-VET', 
+    trailingSlash: true,
     images: {
+    unoptimized: true,
             remotePatterns: [
                 {
                     protocol:'https',
